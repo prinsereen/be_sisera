81bdf0bce1823cf6abaf2e91b3d02357621cd822
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2023-12-08 17:01:08 +0700

    [ADD] update user

--- controllers/Student.js
+++ controllers/Student.js
@@ -1,5 +1,7 @@
 import Student from "../models/StudentModel.js";
-import {success, error} from "../lib/Responser.js"
+import {success, error} from "../lib/Responser.js";
+import bcrypt from "bcrypt"
+import { where } from "sequelize";
 
 export const getAllStudent = async(req, res) => {
     try {
@@ -14,7 +16,7 @@ export const getAllStudent = async(req, res) => {
                 'student_avg_quiz_score',
                 'student_avg_read_score',
                 'student_competiton_recomendation'
-        ]
+            ]
         });
         return success(res, "Berhasil mendapatkan data semua student", student);
     } catch (error) {
@@ -40,9 +42,66 @@ export const getStudentById = async(req, res) => {
                 student_id: req.params.id 
             }
         })
+        if (!student) { return error(res, "Student tidak ditemukan", {})};
         return success(res, "Berhasil mendapatkan data student", student);
     } catch (error) {
         res.status(500).json({ msg: error.message });
     }
 }
+
+
+export const updateStudentById = async (req, res) => {
+    try {
+        const student = await Student.findOne({
+            where: {
+                student_id: req.params.id 
+            },
+            attributes: [
+                'student_id',
+                'student_name',
+                'student_email',
+                'student_class',
+                'student_grade'
+            ]
+        });
+
+        if (!student) {
+            return error(res, "Student tidak ditemukan", {});
+        }
+
+        const { name, email, clas, grade, password, confPassword } = req.body;
+
+        let hashPassword = student.student_password;
+
+        if (password) {
+            const salt = await bcrypt.genSalt();
+            hashPassword = await bcrypt.hash(password, salt);
+        }
+
+        if (password && password !== confPassword) {
+            return error(res, "Password dan confirmation tidak sesuai", {});
+        }
+
+        await Student.update(
+            {
+                student_name: name,
+                student_email: email,
+                student_class: clas,
+                student_grade: grade,
+                student_password: hashPassword
+            }, 
+            {
+                where: {
+                    student_id: req.params.id
+                }
+            }
+        );
+
+        return success(res, "Update berhasil", student);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: error.message });
+    }
+};
+
 
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
-import { getAllStudent, getStudentById } from "../controllers/Student.js";
+import { getAllStudent, getStudentById, updateStudentById } from "../controllers/Student.js";
 
 
 const router = express.Router();
 
 router.get('/student', verifyToken, getAllStudent)
 router.get('/student/:id', verifyToken, getStudentById)
+router.post('/student/:id', verifyToken, updateStudentById)
 
 export default router;
